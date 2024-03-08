b3adaecfd7eaa1ddeecef960cf0befa88838b64c
Aliizi <aliizi1383@gmail.com>
2024-03-08 10:32:10 +0330

    added delete scheduled sends

---
+++ sendsms/delete/delete.go
@@ -0,0 +1,24 @@
+package delete
+
+import "net/http"
+
+var url string = "https://api.sms.ir/v1/send/scheduled/"
+
+func Delete(apikey, packid string) (*http.Response, error) {
+	url = url + packid
+
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("X-API-KEY", apikey)
+	client := new(http.Client)
+	res, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+
+}
